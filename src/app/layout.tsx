da825b427147b94ec6526ce014ba8a66a4d6df41
nibbus <nibbus0x@protonmail.com>
2024-02-09 12:15:19 -0800

    add posthog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Analytics } from '@vercel/analytics/react';
 import { Inter } from "next/font/google";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
--- src/app/template.tsx
+++ src/app/template.tsx
@@ -3,14 +3,32 @@
 import { ChessProvider } from "@/providers/ChessProvider/provider";
 import { PuzzleProvider } from "@/providers/PuzzleProvider/provider";
 import { PropsWithChildren } from "react";
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+
+import dynamic from 'next/dynamic'
+
+const PostHogPageView = dynamic(() => import('../components/PostHogPageView'), {
+  ssr: false,
+})
+
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    capture_pageview: false // Disable automatic pageview capture, as we capture manually
+  })
+}
 
 export default function Template({ children }: PropsWithChildren) {
 
   return (
-    <ChessProvider>
-      <PuzzleProvider>
-        {children}
-      </PuzzleProvider>
-    </ChessProvider>
+    <PostHogProvider client={posthog}>
+      <ChessProvider>
+        <PuzzleProvider>
+          <PostHogPageView />
+          {children}
+        </PuzzleProvider>
+      </ChessProvider>
+    </PostHogProvider>
   );
 }
