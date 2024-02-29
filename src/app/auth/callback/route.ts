eb7829005b03dde927f559b5ddebc965fcef4b32
broskicodes <nibbus0x@protonmail.com>
2024-02-29 15:14:51 -0800

    add code to subscribe emails on sign up (#9)

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from 'next/server'
 import { getSupabaseCilent } from '@/utils/helpers';
+import LoopsClient from 'loops';
+
+const loops = new LoopsClient(process.env.LOOPS_API_KEY!);
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
@@ -10,8 +13,21 @@ export async function GET(request: Request) {
   if (code) {
     const supabase = getSupabaseCilent();
 
-    const { error } = await supabase.auth.exchangeCodeForSession(code);
-    if (!error) {
+    const { error, data: { user } } = await supabase.auth.exchangeCodeForSession(code);
+    if (!error && user) {
+      const [first, last] = user.user_metadata.full_name.replace(" ", "@").split("@");
+
+      const res = await loops.createContact(user.email!, {
+        userId: user.id,
+        firstName: first,
+        lastName: last,
+      })
+
+      if (!res.success && res.message !== "Email or userId is already on list.") {
+        console.log(res);
+        console.error(`Couldn't subscribe user ${user.id} with email ${user.email} to Loops.`);
+      }
+
       console.log('redirecting to', `${origin}${next}`)
       return NextResponse.redirect(`${origin}${next}`)
     }
