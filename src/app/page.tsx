b7b2e07c927531866615558feefc3e45ad1ec7ae
nibbus <nibbus0x@protonmail.com>
2024-03-01 11:54:24 -0800

    small amount of clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ import { GameLogs } from "@/components/GameLogs";
 import { useAuth } from "@/providers/AuthProvider/context";
 import { useChess } from "@/providers/ChessProvider/context";
 import { EvalBar } from "@/components/EvalBar";
+import { Tooltip } from "@/components/Tooltip";
 
 export default function Home() {
   const [showFeedback, setShowFeedback] = useState(false);
@@ -33,9 +34,9 @@ export default function Home() {
         const { data, error } = await supabase.from('feedback').select().eq('uuid', session.id);
 
         if (!data || data.length === 0) {
-          setTimeout(() => {
-            setShowFeedback(true);
-          }, 1000 * 90);
+          // setTimeout(() => {
+          //   setShowFeedback(true);
+          // }, 1000 * 90);
         }
       }
     })();
@@ -65,7 +66,9 @@ export default function Home() {
           <div className="page-content">
             <div className="flex flex-col space-y-4">
               <div className="flex flex-col space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0">
-                <EvalBar />
+                <Tooltip content="Evaluation Bar">
+                  <EvalBar />
+                </Tooltip>
                 <Chessboard />
               </div>
               <BoardControl />
