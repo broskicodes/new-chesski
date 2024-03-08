a1ec02d178d1d5a683177373b08c8a995273741d
nibbus <nibbus0x@protonmail.com>
2024-03-08 12:38:35 -0800

    simple gameplay settings

--- src/providers/StockfishProvider/context.ts
+++ src/providers/StockfishProvider/context.ts
@@ -2,33 +2,29 @@ import { SkillLevel } from "@/utils/types";
 import { createContext, useContext } from "react";
 
 export interface StockfishProviderContext {
-  // stockfish: Stockfish | null;
   isInit: boolean;
   isReady: boolean;
-  // bestMove: string | null;
-  // cp: number;
+  skillLvl: SkillLevel;
   evaluated: boolean;
   initEngine: (limitStrength: boolean, skillLvl?: SkillLevel, moveTime?: number) => void;
+  updateEngine: (limitStrength: boolean, skillLvl?: SkillLevel, moveTime?: number) => void;
   startSearch: () => boolean;
-  // clearBestMove: () => void;
 }
 
 export const StockfishProviderContext = createContext<StockfishProviderContext>({
-  // stockfish: null,
   isInit: false,
   isReady: false,
-  // bestMove: null,
-  // cp: 0,
+  skillLvl: SkillLevel.Beginner,
   evaluated: false,
   initEngine: () => {
+    throw new Error("StockfishProvider not initialized");
+  },
+  updateEngine: () => {
     throw new Error("StockfishProvider not initialized");
   },
   startSearch: () => {
     throw new Error("StockfishProvider not initialized");
   },
-  // clearBestMove: () => {
-  //   throw new Error("StockfishProvider not initialized");
-  // },
 });
 
 export const useStockfish = () => useContext(StockfishProviderContext);
