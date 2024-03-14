2fa999348f955cfbafd9490bc89ab3ca71d4a03b
nibbus <nibbus0x@protonmail.com>
2024-03-13 17:38:07 -0700

    add condition for prod vs prev

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -18,7 +18,9 @@ const PostHogPageView = dynamic(() => import('../components/PostHogPageView'), {
 
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_PROXY_HOST,
+    api_host: process.env.VERCEL_ENV === "production" 
+      ? process.env.NEXT_PUBLIC_POSTHOG_PROD_PROXY_HOST 
+      : process.env.NEXT_PUBLIC_POSTHOG_PREV_PROXY_HOST,
     ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     capture_pageview: false // Disable automatic pageview capture, as we capture manually
   })
