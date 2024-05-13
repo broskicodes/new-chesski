55975b883782fa89cff755f0591c7239b4b0fcd9
nibbus <nibbus0x@protonmail.com>
2024-05-13 14:06:39 -0400

    bugs

--- src/components/ChatPopup.tsx
+++ src/components/ChatPopup.tsx
@@ -6,6 +6,7 @@ import { useAssistant } from "@/providers/AssistantProvider";
 import { ScrollArea } from "./ui/scroll-area";
 import { Badge } from "./ui/badge";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import Link from "next/link";
 
 export const ChatPopup = ({ children }: PropsWithChildren) => {
   const { input, messages, status, handleInputChange, submitMessage, clearChat } = useAssistant();
@@ -33,8 +34,8 @@ export const ChatPopup = ({ children }: PropsWithChildren) => {
       <PopoverAnchor className="fixed bottom-0 right-0" />
       {/* </PopoverAnchor> */}
       <PopoverPortal>
-        <PopoverContent className="flex flex-col h-96 w-80 sm:h-[480px] sm:w-[400px]">
-          <div className="flex flex-row space-x-2 items-center mb-2">
+        <PopoverContent className="flex flex-col h-[420px] w-80 sm:h-[480px] sm:w-[400px]">
+          <div className="flex flex-row space-x-2 items-center mb-1">
             <span className="font-bold">
               Chat
             </span>
@@ -42,17 +43,21 @@ export const ChatPopup = ({ children }: PropsWithChildren) => {
               Beta
             </Badge>
           </div>
+          <div className="text-xs text-gray-500 flex flex-col items-center mb-2">
+            {/* <span>This feature is being tested.</span> */}
+            <span>Please <Link className="underline" href="mailto:braeden@chesski.lol" target="_blank">report bugs</Link>.</span>
+          </div>
           <ScrollArea className="w-full h-full">
             <div className="flex flex-col text-sm space-y-2">
               {messages.map((m, i) => (
-                <div className={`flex flex-row w-full`} style={{ justifyContent: m.role === "user" ? "flex-end" : "flex-start" }}>
+                <div key={i} className={`flex flex-row w-full`} style={{ justifyContent: m.role === "user" ? "flex-end" : "flex-start" }}>
                   {m.role === "assistant" && (
                     <Avatar size="sm">
                       <AvatarImage src="/chesski-logo.svg" />
                       <AvatarFallback>C</AvatarFallback>
                     </Avatar>
                   )}
-                  <div key={i} className={`p-4 rounded-lg w-9/12 ${m.role === "user" ? "bg-indigo-100" : "bg-gray-100"}`}>
+                  <div className={`p-4 rounded-lg w-9/12 ${m.role === "user" ? "bg-indigo-100" : "bg-gray-100"}`}>
                     {m.content}
                   </div>
                 </div>
