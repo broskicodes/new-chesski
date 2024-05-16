e888f0a6393dec4ce773d70133b86026310f9dc6
nibbus <nibbus0x@protonmail.com>
2024-05-16 08:50:13 -0400

    back to old pricing page

--- src/app/subscribe/page.tsx
+++ src/app/subscribe/page.tsx
@@ -20,19 +20,17 @@ import {
   CHESSKI_YEARLY_PRICE,
   SubType,
 } from "@/utils/types";
-import { faBell, faCheck, faCircleCheck, faStar, faUnlock } from "@fortawesome/free-solid-svg-icons";
+import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/navigation";
 import posthog from "posthog-js";
 import React, { Suspense, useEffect, useMemo, useState } from "react";
 import { FreeTrialModal } from "@/components/FreeTrialModal";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { ChatPopupTrigger } from "@/components/ChatPopupTrigger";
 
 const SubPage = () => {
   // const [annual, setAnnual] = useState(true);
-  const [currTab, setCurrTab] = useState("monthly")
-  const [loading, setLoading] = useState(false);
+  const [currTab, setCurrTab] = useState("yearly")
 
   const { isPro } = useUserData();
   const { session, signInWithOAuth } = useAuth();
@@ -49,49 +47,20 @@ const SubPage = () => {
         //   cta: "Monthly billing",
         //   mostPopular: false,
         // },
-        // {
-        //   title: "Pro",
-        //   price: currTab === "yearly" ? Math.round(CHESSKI_YEARLY_PRICE / 12 * 100) / 100 : CHESSKI_MONTHLY_PRICE,
-        //   description: "Start improving for only.",
-        //   promo: "Try free for 3 days",
-        //   features: [
-        //     "Unlimited game and position analysis",
-        //     "Custom AI insights",
-        //     "24hr support from the founder",
-        //     "Access to all new features",
-        //   ],
-        //   cta: "Start 3-day free trial",
-        //   mostPopular: true,
-        // },
         {
-          title: "Trial",
-          description: "Understand your free trial",
+          title: "Pro",
           price: currTab === "yearly" ? Math.round(CHESSKI_YEARLY_PRICE / 12 * 100) / 100 : CHESSKI_MONTHLY_PRICE,
-          steps: [
-            { 
-              h: "Sign up",
-              sh: "You succefully created your account!",
-              i: faCheck,
-              d: true
-            },
-            { 
-              h: "Today: Get Instant Access",
-              sh: "Custom chess insights and explanations generated just for you.",
-              i: faUnlock
-            },
-            {
-              h: "In 2 days: Trial Reminder",
-              sh: "Get an email reminder about when your trials ends. Cancel any time.",
-              i: faBell
-            },
-            {
-              h: "In 3 days: Trial Ends",
-              sh: `Your first payment will be collected on ${new Date(new Date().setDate(new Date().getDate() + 3)).toDateString()}.`,
-              i: faStar
-            }
+          // description: "Unlimited access to all features.",
+          promo: "Try free for 3 days",
+          features: [
+            "Unlimited game and position analysis",
+            "Unlimited custom puzzles",
+            "Custom AI insights",
+            "Access to all new features",
           ],
-          cta: "Start 3-day trial"
-        }
+          cta: "Try for free",
+          mostPopular: true,
+        },
       ],
     }),
     [currTab],
@@ -107,10 +76,8 @@ const SubPage = () => {
   }, []);
 
   return (
-    <div className="pb-8 h-full flex flex-col sm:justify-center sm:items-center overflow-y-auto">
+    <div className="pb-8 h-full flex flex-col justify-center items-center">
       <Navbar showMobile={true} />
-      <ChatPopupTrigger />
-
       {/* <Suspense>
         <FreeTrialModal />
       </Suspense> */}
@@ -125,8 +92,8 @@ const SubPage = () => {
         </div>
       </div> */}
       <Tabs 
-        className="flex flex-col items-center space-y-6 mt-20 sm:mt-0"
-        defaultValue="monthly"
+        className="flex flex-col items-center space-y-6"
+        defaultValue="yearly"
         onValueChange={(val) => {
           setCurrTab(val);
         }} >
@@ -138,39 +105,19 @@ const SubPage = () => {
           <TabsContent key={type} value={type}>
             {pricing.tiers.map((tier) => (
               <div key={tier.title} className="flex flex-col items-center">
-                <span className="font-bold text-3xl sm:text-4xl font-m mb-8 text-center">{tier.description}</span>
-                <ul role="list" className="mb-8 space-y-6 text-sm w-full px-8 sm:w-96 sm:px-0">
-                  {tier.steps.map((step) => (
-                    <li key={step.h} className="grid grid-cols-12 gap-x-4 items-center">
-                      <div className={`place-self-center rounded-full p-3 shadow ${!step.d ? "bg-white" : "bg-[#1b03a3]/50"}`}>
-                        <FontAwesomeIcon
-                          icon={step.i}
-                          size="xl"
-                          className={`flex-shrink-0  ${step.d ? "text-white" : "text-[#1b03a3]"}`}
-                          aria-hidden="true"
-                        />
-                      </div>
-                      <div className="flex flex-col ml-2 col-span-11">
-                        <span className={`text-xl font-bold text-[#1b03a3] ${step.d ? "opacity-70 line-through decoration-1" : ""}`}>{step.h}</span>
-                        <span className="text-gray-500">{step.sh}</span>
-                        </div>
-                    </li>
-                  ))}
-                </ul>
-                <div className="mb-2 flex flex-col items-center font-m">
-                  <div className="font-light">3-day free trial, then</div>
-                  <div className="space-x-1">
-                    {type === "yearly" && <span className="font-semibold">${Math.round(tier.price * 12) } /year</span>}
-                    <span className={`${type === "yearly" ? "" : "font-semibold"}`}>{type === "yearly" && "("}${tier.price} /month{type === "yearly" && ")"}</span>
-                  </div>
+                <div className="font-bold text-2xl sm:text-4xl font-m mb-8 text-center flex flex-col">
+                  <span className="whitespace-nowrap">Your very own chess coach!</span> 
+                  <span className="whitespace-nowrap font-medium">for only</span>
+                </div>
+                <div className="mb-6 flex flex-col items-center">
+                  <div className="font-m"><span className="text-6xl font-semibold text-[#1b03a3]">${tier.price}</span> /month</div>
+                  {type === "yearly" && <CardDescription className="text-lg font-m">billed anually</CardDescription>}
                 </div>
                 {session && !isPro && (
                   <Button
                     size="lg"
                     className="w-60"
-                    disabled={loading}
                     onClick={async () => {
-                      setLoading(true)
                       posthog.capture("sub_clicked");
                       const re = await fetch("/api/stripe/checkout/session", {
                         method: "POST",
@@ -187,31 +134,39 @@ const SubPage = () => {
                       router.push(link);
                     }}
                   >
-                    {!loading && <span className="font-bold text-xl">{tier.cta}</span>}
-                    {loading && <div className="animate-spin rounded-full h-4 w-4 border-b-2 border-[#1B03A3]" />}
+                    <span className="font-bold text-xl">{tier.cta}</span>
                   </Button>
                 )}
                 {session && isPro && (
                   <Button
                     disabled
-                    size="lg"
                     className="w-60 disabled:opacity-70 disabled:bg-[#1b03a3] disabled:ring-gred:ring-4"
                   >
-                    <span className="font-bold text-xl">Subscribed</span>
+                    Subscibed
                   </Button>
                 )}
                 {!session && (
                   <Button
-                    size="lg"
                     className="w-60"
                     onClick={() => {
                       signInWithOAuth("subscribe");
                     }}
                   >
-                    <span className="font-bold text-xl">Sign Up</span>
+                    Sign Up
                   </Button>
                 )}
-                
+                <ul role="list" className="mt-6 space-y-2 text-sm">
+                  {tier.features.map((feature) => (
+                    <li key={feature} className="flex">
+                      <FontAwesomeIcon
+                        icon={faCircleCheck}
+                        className="flex-shrink-0 w-4 h-4 text-indigo-500"
+                        aria-hidden="true"
+                      />
+                      <span className="ml-2 text-gray-500">{feature}</span>
+                    </li>
+                  ))}
+                </ul>
               </div>
               // <Card
               //   key={tier.title}
